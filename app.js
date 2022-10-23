9e2220a5907976c72620d9592e8d8d4572e87004
Stephen Ubong <stevelov1982@gmail.com>
2022-10-23 09:35:41 +0100

    Express Sessions Part 2

--- app.js
+++ app.js
@@ -38,48 +38,28 @@ app.use(session({
    name: 'session-id',
    secret: '12345-67890-09876-54321',
    saveUninitialized: false,
+   resave: false,
    store: new FileStore()
 }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-
-    if (!authHeader) {
       var err = new Error('You are not authenticated');
-  
-      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next(err);
-    }
-  
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  
-    var username = auth[0];
-    var password = auth[1];
-  
-    if (username === 'admin' && password === 'password') {
-      req.session.user = 'admin';
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated');
-  
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
   }
   else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
       var err = new Error('You are not authenticated');
-  
-      err.status = 401;
+        err.status = 403;
       return next(err);
     }
   }
@@ -90,8 +70,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
