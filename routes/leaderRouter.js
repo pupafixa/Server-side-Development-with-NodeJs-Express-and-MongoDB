a6df2f00998bed425b41f5e06e2dffd581ab165f
Stephen Ubong <stevelov1982@gmail.com>
2022-10-25 20:05:10 +0100

    Passport JWT

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const authenticate = require('../authenticate');
 const Leaders = require('../models/leaders');
-
 
 const leaderRouter = express.Router();
 
@@ -19,7 +18,7 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req,res,next) => {
+.post(authenticate.verifyUser, (req,res,next) => {
     Leaders.create(req.body)
     .then((leader) => {
       console.log('Leader Created', leader);
@@ -29,11 +28,11 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req,res,next) => {
+.put(authenticate.verifyUser, (req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /leaders');
 })
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser, (req,res,next) => {
     Leaders.remove({})
     .then((resp) => {
       res.statusCode = 200;
@@ -42,6 +41,7 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 });
+
 leaderRouter.route('/:leaderId')
 .get((req,res,next) => {
     Leaders.findById(req.params.leaderId)
@@ -52,10 +52,10 @@ leaderRouter.route('/:leaderId')
       }, (err) => next(err))
       .catch((err) => next(err));
 })
-.post((req,res,next) => {
+.post(authenticate.verifyUser, (req,res,next) => {
     res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
  })
- .put((req,res,next) => {
+ .put(authenticate.verifyUser, (req,res,next) => {
     Leaders.findByIdAndUpdate(req,params.leaderId, {
      $set: req.body
     }, { new: true })
@@ -66,7 +66,7 @@ leaderRouter.route('/:leaderId')
    }, (err) => next(err))
    .catch((err) => next(err));
  })
- .delete((req,res,next) => {
+ .delete(authenticate.verifyUser, (req,res,next) => {
     Leaders.findByIdAndRemove(req,params.leaderId)
     .then((resp) => {
      res.statusCode = 200;
